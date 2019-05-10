df4349199eab2e62dbc4feb2246f35aff1f570ac
ashwbel <ashwag.wabel11@gmail.com>
2019-05-10 20:19:19 +0300

    create variables height ,width ,color

--- designs.js
+++ designs.js
@@ -1,6 +1,12 @@
 
 // Select color input
+var color
+color  = document.getElementById('colorPicker')
+
 // Select size input
+var height ,width
+height = document.getElementById('inputHeight');
+width = document.getElementById('inputWidth');
 
 
 
