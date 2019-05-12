1e0e9b0706fbd25e5b3787eb9a58002cad22d27c
ashwbel <ashwag.wabel11@gmail.com>
2019-05-12 20:19:19 +0300

    Add Function MakeGrid

--- designs.js
+++ designs.js
@@ -20,6 +20,24 @@ submitbtn.addEventListener('submit', function (event) {
 
 
 
-function makeGrid() {
+function makeGrid(x,y) {
 
+   event.preventDefault();
+
+
+// Your code goes here!
+
+  while (pixelCanvas.firstChild) {
+    pixelCanvas.removeChild(pixelCanvas.firstChild);
+  }
+  // Creates Grid
+  for (let i = 1; i <= x; i++) {
+    let rw = document.createElement('tr');
+    pixelCanvas.appendChild(rw);
+    for (let j = 1; j <= y; j++) {
+      let cl = document.createElement('td');
+      rw.appendChild(cl);
+       cl.addEventListener("click", addcolor);
+     }
+  }
 }
